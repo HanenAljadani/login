a4d606c6c92294c6db41f3d65bba722f803a5c32
Haneen J <haneenj@MacBook-Air-alkhas-b-Haneen-3.local>
2022-08-16 09:44:53 +0300

    test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'login';
+  loginForm!: FormGroup;
+
+  ngOnInit(){
+     this.loginForm = new FormGroup({
+      'email': new FormControl(null, [Validators.email , Validators.required]),
+      'password' : new FormControl(null ,[Validators.required])
+     })
+  }
+
+
 }
